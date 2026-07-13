5816d1d8dbd3ab3aef91658561e71794dad6e6fe
MateuszRuczaj1 <matrucz@outlook.com>
2026-07-13 19:19:53 +0200

    Create htmlnode

---
+++ src/htmlnode.py
@@ -0,0 +1,19 @@
+class HTMLNode():
+    def __init__(self, tag = None, value=None, children=None, props: dict=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    def to_html(self):
+        raise NotImplementedError
+    def props_to_html(self):
+        if self.props:
+            string = " "
+            for key, value in self.props.items():
+                string+=f"{key}={value} "
+            return string
+        else:
+            return ""
+    def __repr__(self):
+        return f"Tag: {self.tag}\nValue: {self.value}\nChildren: {self.children}\nProps: {self.props_to_html()}"
+    
--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 from textnode import TextNode
 from textnode import TextType
+from htmlnode import HTMLNode
 def main():
     text = TextNode('This is some anchor text', TextType.LINK, 'https://www.boot.dev')
+    html_node = HTMLNode("h1", "This is main header", "p", {
+        "color":"red",
+        "href": "https://google.com/"
+    })
     print(text)
+    print(html_node)
 main()
