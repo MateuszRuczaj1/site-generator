acf695d202ad9416604cb7c462eaf371ef25261a
MateuszRuczaj1 <matrucz@outlook.com>
2026-07-17 22:20:12 +0200

    Revert "add more test cases"
    This reverts commit a9a17e0fc12ae77986ed9f7685f4283d36ecff38.

--- src/test_splitnodes.py
+++ src/test_splitnodes.py
@@ -11,37 +11,6 @@ class TestSplitNodes(unittest.TestCase):
         node = TextNode("String `Code` String", TextType.TEXT)
         actual = split_nodes([node], "`", TextType.CODE_TEXT)
         self.assertEqual(expected, actual)
-    def test_split_nodes_with_bold(self):
-        expected = [
-        TextNode("Regular text part 1 ",TextType.TEXT,None),
-        TextNode("BOLD!!!", TextType.BOLD,None),
-        TextNode(" Regular text part 2", TextType.TEXT,None)
-        ]
-        node = TextNode("Regular text part 1 **BOLD!!!** Regular text part 2", TextType.TEXT)
-        actual = split_nodes([node], "**", TextType.BOLD)
-        self.assertEqual(expected, actual)
-    def test_split_nodes_with_italic(self):
-        expected = [
-            TextNode("Lorem ipsum ", TextType.TEXT, None),
-            TextNode("dolor", TextType.ITALIC_TEXT, None),
-            TextNode(" sit amet", TextType.TEXT, None)
-        ]
-        node = TextNode("Lorem ipsum _dolor_ sit amet", TextType.TEXT)
-        actual = split_nodes([node], "_", TextType.ITALIC_TEXT)
-        self.assertEqual(expected, actual)
-    
-    def test_split_multiple_nodes_with_italic(self):
-        expected = [
-            TextNode("Lorem ipsum ", TextType.TEXT, None),
-            TextNode("dolor", TextType.ITALIC_TEXT, None),
-            TextNode(" sit amet, ", TextType.TEXT, None),
-            TextNode("consectetur", TextType.ITALIC_TEXT, None),
-            TextNode(" adipiscing elit.", TextType.TEXT, None)
-        ]
-        node = TextNode("Lorem ipsum _dolor_ sit amet, ", TextType.TEXT)
-        node_2 = TextNode("_consectetur_ adipiscing elit.", TextType.TEXT)
-        actual = split_nodes([node, node_2], "_", TextType.ITALIC_TEXT)
-        self.assertEqual(expected, actual)
 
 if __name__ == "__main__":
     unittest.main()
