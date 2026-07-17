5c31bb0d1ecf9bf4bf24278b6458dda29b99735e
MateuszRuczaj1 <matrucz@outlook.com>
2026-07-17 22:13:23 +0200

    fix bug a that creates empty textnodes if 'special' node is either first or last one'

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -2,15 +2,23 @@ from textnode import TextNode, TextType
 def split_nodes(old_nodes: list[TextNode], delimiter: str, text_type:TextType) -> TextNode:
     new_nodes = []
     for node in old_nodes:
-        print(node.text)
+       
         if node.text_type.name != "TEXT":
             new_nodes.append(node)
+            continue
         node_split = node.text.split(delimiter)
-        print(node_split)
         if len(node_split) % 2 == 0:
             raise Exception(f"Invalid markdown syntax: {delimiter} closing is not found")
-        nodes_to_add = [TextNode(node,TextType.TEXT) if i % 2 == 0 else TextNode(node, text_type) for i, node in enumerate(node_split)]
-        print(nodes_to_add)
+        nodes_to_add = []
+        for i, text in enumerate(node_split):
+            if text=="":
+                continue
+            if i % 2 == 0:
+                nodes_to_add.append(TextNode(text, TextType.TEXT))
+            else:
+                nodes_to_add.append(TextNode(text, text_type))
+
+        
         new_nodes.extend(nodes_to_add)
         
     
